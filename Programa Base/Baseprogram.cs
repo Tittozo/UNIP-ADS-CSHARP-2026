143ae6263b6129124dfcc41c0b0b8a003857e55a
Mateus Antunes <mateusetec424@gmail.com>
2026-02-25 20:08:32 -0300

    Create Baseprogram.cs

---
+++ Programa Base/Baseprogram.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
