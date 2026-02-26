dcdc16ffcb8a28ac62cc40b6ffca41e900d5e993
Mateus Antunes <mateusetec424@gmail.com>
2026-02-25 21:19:56 -0300

    Update Baseprogram.cs

--- Programa Base/Baseprogram.cs
+++ Programa Base/Baseprogram.cs
@@ -10,3 +10,32 @@ namespace Aula1
         }
     }
 }
+
+//TESTANDO TIPOS DE DADOS
+
+using System;
+
+namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int valor = 1987;
+            float valorfloat = 3.14f;
+            double valordouble = 3.14159265;
+            string nome = "Mateus";
+            char Letra = 'M';
+            long valorlong = 123123123L;
+
+            Console.WriteLine(valor);
+            Console.WriteLine(nome);
+            Console.WriteLine(Letra);
+            Console.WriteLine(valorlong);
+            Console.WriteLine(valorfloat);
+            Console.WriteLine(valordouble);
+        }
+    }
+}
