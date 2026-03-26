016529c61df4015c605097ba18c7838c64464277
Mateus Antunes <mateusetec424@gmail.com>
2026-03-26 13:24:19 -0300

    Update Program.cs

--- Animal/Program.cs
+++ Animal/Program.cs
@@ -1,1 +1,26 @@
+namespace Animal
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Animal gato = new Animal();
+
+            gato.Nome = "Tobis";
+
+            Console.WriteLine(gato.Nome);
+            gato.EmitirSom();
+            gato.Comer();
+
+            Cachorro dog = new Cachorro("Bob", 10, "Puddle", "M");
+            dog.EmitirSom();
+            dog.Comer();
+
+            Tubarao shark = new Tubarao();
+            shark.EmitirSom();
+            shark.Comer();
+
+        }
+    }
+}
 
