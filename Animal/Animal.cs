707194807dee492c2dad9ebe2f269a9e83ee907c
Mateus Antunes <mateusetec424@gmail.com>
2026-03-26 13:24:41 -0300

    Update Animal.cs

--- Animal/Animal.cs
+++ Animal/Animal.cs
@@ -1,1 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace Animal
+{
+    internal class Animal
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double Peso { get; set; }
+        public string Especie { get; set; }
+
+        public Animal(string nome, int idade, double peso, string especie)
+        {
+            Nome = nome;
+            Idade = idade;
+            Peso = peso;
+            Especie = especie;
+        }
+
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine("Se prepara para emitir som");
+            Console.WriteLine("Som do animal");
+        }
+
+        public virtual void Comer()
+        {
+            Console.WriteLine("O animal está comendo");
+        }
+    }
+}
