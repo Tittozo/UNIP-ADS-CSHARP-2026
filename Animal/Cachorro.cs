50705418c8527e4571ed84e3b1d6e787ed300b9d
Mateus Antunes <mateusetec424@gmail.com>
2026-03-30 21:19:31 -0300

    Update Cachorro.cs

--- Animal/Cachorro.cs
+++ Animal/Cachorro.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    internal class Cachorro: Animal
+    {
+        public char Sexo { get;  set; }
+        
+        public Cachorro (string nome, int idade, double peso, string especie, char sexo): base(nome, idade, peso, especie)
+        {
+            Sexo = sexo;
+        }
+        public override void EmitirSom()
+        {
+            base.EmitirSom();
+            Console.WriteLine("Latino");
+        }
+
+        public override void Comer()
+        {
+            Console.WriteLine("Comendo ração dos racãocionais");
+        }
+    }
+}
 
