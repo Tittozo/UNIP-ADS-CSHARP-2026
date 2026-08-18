7356a7232edc34b7f809e3d4d8cfc3b461e1c9f5
Tittozo <mateusetec424@gmail.com>
2026-08-18 13:22:10 -0300

    Adicionar arquivos de projeto.

---
+++ TypePatternV2/Desenvolvedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace TypePatternV2
+{
+    internal class Desenvolvedor : Funcionario
+    {
+        public Desenvolvedor(string nome, int idade, string linguagem, int anosExperiencia) : base(nome, idade)
+        {
+            this.Linguagem = linguagem;
+            this.AnosExperiencia = anosExperiencia;
+        }
+
+        public string Linguagem { get; set; }
+        public int AnosExperiencia { get; set; }
+    }
+}
---
+++ TypePatternV2/Estagiario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypePatternV2
+{
+    internal class Estagiario : Funcionario
+    {
+        public Estagiario(string nome, int idade, int horasSemanais) : base(nome, idade)
+        {
+            this.HorasSemanais = horasSemanais;
+        }
+
+        public int HorasSemanais { get; set; }
+    }
+}
