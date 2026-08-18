703666fcb5c1a83d840477716e6a352594937277
Tittozo <mateusetec424@gmail.com>
2026-08-18 13:55:28 -0300

    Adicionar PROJETO

---
+++ FirstApp(MVC) C#/Controllers/EnfermeiraController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp_MVC__C_.Controllers
+{
+    public class EnfermeiraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Nome { get; set; }
+
+        public string Idade { get; set; }
+    }
+}
